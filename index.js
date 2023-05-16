f7c5e1aa797033184d1a763e2108f9915999fe9e
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-05-16 19:54:55 +0530

    Addition of CORS
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://paradox-23.ieeecsvitc.com');
     next();
 });
 
